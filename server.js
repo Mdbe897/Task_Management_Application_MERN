8b5b09995a232671d98a8907222b865196057157
mdbe897 <b.mohamed897@gmail.com>
2026-04-22 19:17:53 +0100

    Phase 2: User authentication with JWT

--- server.js
+++ server.js
@@ -2,19 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
-// middleware
+// ✅ middleware FIRST
 app.use(express.json());
 app.use(cors());
 
 // connect DB
 connectDB();
 
-// routes
+// ✅ routes AFTER middleware
+app.use("/api/auth", authRoutes);
+
 app.use("/api/tasks", (req, res) => {
   res.send("Task route working 🚀");
 });
